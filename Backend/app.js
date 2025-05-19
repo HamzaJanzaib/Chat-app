3612eb8b03c4b477d2ec7a9d88caf00e84cec2b8
Hamzajabzaib <hamza janzaib>
2025-05-19 16:45:17 +0500

    config mongodb

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,23 @@
-import express from "express"
-import Cors from "cors"
-import dotenv from "dotenv"
-dotenv.config()
+import express from 'express';
+import Cors from 'cors';
+import connectDB from "./ConnectDB.js";
 
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({limit : "4mb", extended : true}))
-app.use(Cors())
+import dotenv from 'dotenv';
+dotenv.config();
 
 
+if (!process.env.MONGO_URL) {
+    console.error("MONGO_URL is not defined in the environment variables");
+    process.exit(1);
+}
+
+// Connect to MongoDB
+connectDB(process.env.MONGO_URL);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ limit: "4mb", extended: true }));
+app.use(Cors());
 
 export default app
